6bc71a944f3766752b49df648abdf631ac1fab4a
Heinke Lubbe <lubbeheinke7@gmail.com>
2021-08-26 09:05:49 +0200

    Basic Swagger

---
+++ AccountSystemDomain/src/main/java/za/ac/nwu/as/domain/service/GeneralResponse.java
@@ -0,0 +1,38 @@
+package za.ac.nwu.as.domain.service;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class GeneralResponse<T> implements Serializable {
+
+    private static final long serialVersionUID = 736511190197986812L;
+    private final boolean successful;
+    private final transient T payload;
+
+    public GeneralResponse(boolean successful, T payload){
+        this.successful = successful;
+        this.payload = payload;
+    }
+
+    public boolean isSuccessful() {return successful;}
+
+    public T getPayload() {return payload;}
+
+    public boolean equals(Object o){
+        if (this ==o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GeneralResponse<?> that = (GeneralResponse<?>) o;
+        return successful == that.successful && Objects.equals(payload, that.payload);
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(successful,payload);}
+
+    @Override
+    public String toString(){
+        return "GeneralResponse{" +
+                "successfull=" + successful +
+                ", payload= " + payload +
+                "}";
+    }
+}
