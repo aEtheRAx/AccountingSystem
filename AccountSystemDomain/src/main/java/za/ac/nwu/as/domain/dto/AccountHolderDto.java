41cf9dabcb5438e8b0a8a24719276d58d0aa809e
aEtheRAx <lubbeheinke7@gmail.com>
2021-09-23 13:38:15 +0200

    DTO's Added

---
+++ AccountSystemDomain/src/main/java/za/ac/nwu/as/domain/dto/AccountHolderDto.java
@@ -0,0 +1,120 @@
+package za.ac.nwu.as.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import za.ac.nwu.as.domain.persistence.AccountHolder;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@ApiModel(value = "AccountHolder", description = "A DTO that represents the AccountHolder (member)")
+
+public class AccountHolderDto implements Serializable {
+
+    private static final long serialVersionUID = -5566983671858386439L;
+    private String memberName;
+    private Long balance;
+    private String currency;
+    private LocalDate startDate;
+
+    public AccountHolderDto() {
+    }
+
+    public AccountHolderDto(String memberName, Long balance, String currency, LocalDate startDate) {
+        this.memberName = memberName;
+        this.balance = balance;
+        this.currency = currency;
+        this.startDate = startDate;
+    }
+
+    public AccountHolderDto(AccountHolder accountHolder) {
+        this.setMemberName(accountHolder.getMemberName());
+        this.setBalance(accountHolder.getBalance());
+        this.setCurrency(accountHolder.getCurrency());
+        this.setStartDate(accountHolder.getStartDate());
+    }
+
+    @ApiModelProperty(position = 1,
+            value = "AccountHolder Name",
+            name = "Name",
+            notes = "The name of the AccountHolder",
+            dataType = "java.lang.String",
+            example = "John",
+            required = true)
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    @ApiModelProperty(position = 2,
+            value = "AccountHolder Balance",
+            name = "Balance",
+            notes = "The balance of the AccountHolder",
+            dataType = "java.lang.Long",
+            example = "5000",
+            required = true)
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Long balance) {
+        this.balance = balance;
+    }
+
+    @ApiModelProperty(position = 3,
+            value = "AccountHolder Currency",
+            name = "Currency",
+            notes = "The currency of the AccountHolder",
+            dataType = "java.lang.String",
+            example = "MILES",
+            required = true)
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @ApiModelProperty(position = 3,
+            value = "AccountHolder Start Date",
+            name = "StartDate",
+            notes = "This is the start date on which to add Miles/Currency for a member",
+            dataType = "java.lang.String",
+            example = "2020-01-01",
+            allowEmptyValue = true)
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountHolderDto that = (AccountHolderDto) o;
+        return Objects.equals(memberName, that.memberName) && Objects.equals(balance, that.balance) && Objects.equals(currency, that.currency) && Objects.equals(startDate, that.startDate);
+    }
+
+    @JsonIgnore
+    public AccountHolder getAccountHolder() {
+        return new AccountHolder(getMemberName(), getBalance(), getCurrency(), getStartDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(memberName, balance, currency, startDate);
+    }
+}
---
+++ AccountSystemDomain/src/main/java/za/ac/nwu/as/domain/dto/AccountTransactionDto.java
@@ -0,0 +1,134 @@
+package za.ac.nwu.as.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import za.ac.nwu.as.domain.persistence.AccountTransaction;
+import za.ac.nwu.as.domain.persistence.AccountTransactionDetails;
+import za.ac.nwu.as.domain.persistence.AccountType;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class AccountTransactionDto implements Serializable {
+
+    private static final long serialVersionUID = -4574129268923320303L;
+    private Long transactionId;
+    private String accountTypeMnemonic;
+    private Long memberId;
+    private Long amount;
+    private LocalDate transactionDate;
+    private AccountTransactionDetails details;
+
+    public AccountTransactionDto() {
+    }
+
+    public AccountTransactionDto(Long transactionId, String accountTypeMnemonic, Long memberId, Long amount, LocalDate transactionDate) {
+        this.transactionId = transactionId;
+        this.accountTypeMnemonic = accountTypeMnemonic;
+        this.memberId = memberId;
+        this.amount = amount;
+        this.transactionDate = transactionDate;
+    }
+
+    public AccountTransactionDto(Long transactionId, String accountTypeMnemonic, Long memberId, Long amount, LocalDate transactionDate, AccountTransactionDetails details) {
+        this.transactionId = transactionId;
+        this.accountTypeMnemonic = accountTypeMnemonic;
+        this.memberId = memberId;
+        this.amount = amount;
+        this.transactionDate = transactionDate;
+        this.details = details;
+    }
+
+    @JsonIgnore
+    public AccountTransactionDto(AccountTransaction accountTransaction) {
+        this.transactionId = accountTransaction.getTransactionId();
+        this.accountTypeMnemonic = accountTransaction.getAccountType().getMnemonic();
+        this.memberId = accountTransaction.getMemberId();
+        this.amount = accountTransaction.getAmount();
+        this.transactionDate = accountTransaction.getTransactionDate();
+        if (null != accountTransaction.getDetails()) {
+            //this.details = new AccountTransactionDetailsDto(accountTransaction.getDetails());
+            this.details = accountTransaction.getDetails();
+        }
+    }
+
+    @JsonIgnore
+    public AccountTransaction buildAccountTransaction(AccountType accountType) {
+        return new AccountTransaction(this.getTransactionId(), accountType, this.getMemberId(),
+                this.getAmount(), this.getTransactionDate());
+    }
+
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public String getAccountTypeMnemonic() {
+        return accountTypeMnemonic;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public AccountTransactionDetails getDetails() {
+        return details;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public void setAccountTypeMnemonic(String accountTypeMnemonic) {
+        this.accountTypeMnemonic = accountTypeMnemonic;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public void setDetails(AccountTransactionDetails details) {
+        this.details = details;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountTransactionDto that = (AccountTransactionDto) o;
+        return Objects.equals(transactionId, that.transactionId) && Objects.equals(accountTypeMnemonic, that.accountTypeMnemonic) && Objects.equals(memberId, that.memberId) && Objects.equals(amount, that.amount) && Objects.equals(transactionDate, that.transactionDate) && Objects.equals(details, that.details);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transactionId, accountTypeMnemonic, memberId, amount, transactionDate, details);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountTransactionDto{" +
+                "transactionId=" + transactionId +
+                ", accountTypeMnemonic='" + accountTypeMnemonic + '\'' +
+                ", memberId=" + memberId +
+                ", amount=" + amount +
+                ", transactionDate=" + transactionDate +
+                ", details=" + details +
+                '}';
+    }
+}
+
